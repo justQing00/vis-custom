45e1872a75f2d1775a07aec3f2bc777471cddeff
tanliqing <qing1115812681@gmail.com>
2017-01-09 13:38:32 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var path=require('path');
+
+module.exports= {
+  entry:{
+    index:"./index.js",
+  },
+  output:{
+    path: path.join(__dirname,'public'),
+    publicPath: "/public/",
+    filename: "[name].js",
+    chunkFilename: "[id].chunk.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader?presets[]=es2015'
+      }
+    ]
+  }
+}
